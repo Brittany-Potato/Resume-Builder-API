97d9fa75a8d8c686b88477c58c9e31ee57941fec
brittany-potato <brittanycc@missionreadyhq.com>
2025-07-16 17:11:38 +1200

    registrationroute file added and mongodb connection created

---
+++ registrationRoute.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('./user');
+
+// POST /auth/register
+router.post('./register', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Hash + salt the password
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // Create user with hashed password
+        const newUser = new User({ email, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({ message: 'User registered successfully!'});
+    } catch (err) {
+        res.statue(500).json({ error: 'Registration failed', details: err.message });
+    }
+});
--- server.js
+++ server.js
@@ -2,15 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const passport =  require('passport');
-require('.config/passport')(passport);
 
 const app = express();
 app.use(express.json());
 app.use(passport.initialize());
 
-mongoose.connect(process.env.MONGO_URI);
+require('./config/passport')(passport);
 
-app.use('auth', require('./routes/auth'));
+mongoose.connect(process.env.MONGO_URI, {
+    newUserUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err.message));
+
+app.use('auth', require('./registrationRoute'));
 app.use('./resume', require('./routes/resume'));
 
 app.listen(3000, () => console.log('Server running on port 3000'));
